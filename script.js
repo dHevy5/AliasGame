7cfa1459cf555ae43ea287f10863511bd37bcb51
dHevy5 <noartsol911@mail.ru>
2026-03-06 00:12:31 +0400

    mobileFix3

--- script.js
+++ script.js
@@ -8,6 +8,22 @@ const wordsDatabase = {
     cinema: ['Фильм', 'Актер', 'Режиссер', 'Кинотеатр', 'Попкорн', 'Сценарий', 'Камера', 'Звезда', 'Премия', 'Драма']
 };
 
+document.addEventListener('gesturestart', function(e) {
+    e.preventDefault();
+});
+
+document.addEventListener('touchmove', function(e) {
+    if (e.scale !== 1) {
+        e.preventDefault();
+    }
+}, { passive: false });
+
+document.addEventListener('wheel', function(e) {
+    if (e.ctrlKey) {
+        e.preventDefault();
+    }
+}, { passive: false });
+
 let canClick = true;
 
 // Состояние приложения
